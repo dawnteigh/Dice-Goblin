6995797608e41b680e7d9ce5df71c0e4f7aaf5e6
Donte Ladatto <donte279@gmail.com>
2022-06-17 13:23:48 -0500

    create React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.seekpng.com/png/full/13-139137_vector-d20-png-image-transparent-dungeons-and-dragons.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
