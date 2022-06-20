a1840ca8d0474d483cab2387c198f3f42584449f
Donte Ladatto <donte279@gmail.com>
2022-06-20 17:06:32 -0500

    add routing and navlinks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import NavBar from './NavBar';
 import Home from './Home';
 import Dice from './Dice';
 import Stats from './Stats';
+import { Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <Dice />
-      <Stats />
+      <Switch>
+      <Route exact path="/dice" render={ (props) =>  <Dice { ...props }/>} />
+      <Route exact path="/stats" render={ (props) =>   <Stats { ...props }/>} />
+      <Route exact path="/" render={ (props) => <Home { ...props }/>} />
+      </Switch>
     </div>
   );
 }
--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+
 
 const NavBar = () => {
+
+  const active = { background: "#ee07e2",
+  boxShadow: "rgba(238, 7, 226, 0.35) 0 -25px 18px -14px inset, rgba(238, 7, 226, 0.35) 0 1px 2px, rgba(238, 7, 226, 0.35) 0 2px 4px, rgba(238, 7, 226, 0.35) 0 4px 8px, rgba(238, 7, 226, 0.35) 0 8px 16px, rgba(238, 7, 226, 0.35) 0 16px 32px" }
+  
+
   return (
-    <div>NavBar</div>
+    <div>
+      <NavLink
+        to="/"
+        exact
+        className="navTab"
+        activeStyle={active}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/dice"
+        exact
+        className="navTab"
+        activeStyle={active}
+      >
+        Dice
+      </NavLink>
+      <NavLink
+        to="/stats"
+        exact
+        className="navTab"
+        activeStyle={active}
+      >
+        Stats
+      </NavLink>
+    </div>
   )
 }
 
