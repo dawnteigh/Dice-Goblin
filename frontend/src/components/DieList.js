8acbde57f584596894fd424c913a9c6e26d7b788
Donte Ladatto <donte279@gmail.com>
2022-06-22 18:02:58 -0500

    Add filter by type to DieList

--- frontend/src/components/DieList.js
+++ frontend/src/components/DieList.js
@@ -7,7 +7,7 @@ import DieCard from './DieCard'
 const DieList = ({ dice }) => {
 
   const [list, setList] = useState([])
-  // const [filter, setFilter] = useState("all")
+  const [filter, setFilter] = useState("all")
   const [search, setSearch] = useState("")
 
   useEffect(()=> {
@@ -22,9 +22,30 @@ const DieList = ({ dice }) => {
     )
   })
 
+  const handleFilterChange = (e) => {
+    if (e.target.value === "all") {
+      setList(dice)
+    } else {
+      const filteredDice = dice.filter(d => d.type_of_die === e.target.value)
+      setList(filteredDice)
+    }
+    setFilter(e.target.value)
+  }
+
   return (
     <div>
-      <input onChange={(e) => setSearch(e.target.value)} type="text" size="45" placeholder="Search dice by description" />
+      <input onChange={(e) => setSearch(e.target.value)} type="text" size="60" placeholder="Search dice by description" />
+      <select onChange={handleFilterChange}>
+        <option value="all">All Dice</option>
+        <option value="d4">d4</option>
+        <option value="d6">d6</option>
+        <option value="d8">d8</option>
+        <option value="d10">d10</option>
+        <option value="d%">d%</option>
+        <option value="d12">d12</option>
+        <option value="d20">d20</option>
+        <option value="2d6">2d6</option>
+      </select>
       <div className="diceGrid">
           {diceList}
       </div>
