844dcb3a6f7fc62bedf697b3db7a5a981da53a60
Donte Ladatto <donte279@gmail.com>
2022-07-05 14:21:19 -0500

    Handle empty stats

--- backend/app/models/die.rb
+++ backend/app/models/die.rb
@@ -45,27 +45,30 @@ class Die < ActiveRecord::Base
     def self.seven_percentage
         sevens = Die.all.where(type_of_die: "2d6").map{ |d| d.values.where(value: 7) }.map{ |v| v[0].times_rolled }.sum
         craps_rolls = Die.all.where(type_of_die: "2d6").map{ |d| d.total_rolls }.sum
-        ((sevens / craps_rolls.to_f) * 100).round(2)
+        percentage = ((sevens / craps_rolls.to_f) * 100).round(2)
     end
 
     def self.type_stats(type_of_die)
         qualifying_dice = Die.all.filter{ |d| d.type_of_die == type_of_die && d.total_rolls >= 50 }
-        averages = qualifying_dice.map{ |d| d.average_roll }
-        max_avg_name = qualifying_dice.find{ |d| d.average_roll == averages.max }.description
-        min_avg_name = qualifying_dice.find{ |d| d.average_roll == averages.min }.description
-        top_max_perc = qualifying_dice.map{ |d| ((d.values.last.times_rolled / d.total_rolls.to_f) * 100).round(2) }.max
-        top_min_perc = qualifying_dice.map{ |d| ((d.values.first.times_rolled / d.total_rolls.to_f) * 100).round(2) }.max
-        top_max_name = qualifying_dice.find{ |d| ((d.values.last.times_rolled / d.total_rolls.to_f) * 100).round(2) == top_max_perc }.description
-        top_min_name = qualifying_dice.find{ |d| ((d.values.first.times_rolled / d.total_rolls.to_f) * 100).round(2) == top_min_perc }.description
-        { 
-            max_avg: averages.max,
-            max_avg_name: max_avg_name,
-            min_avg: averages.min,
-            min_avg_name: min_avg_name,
-            max_perc: top_max_perc,
-            max_perc_name: top_max_name,
-            min_perc: top_min_perc,
-            min_perc_name: top_min_name
-        }
+        check = qualifying_dice.empty?
+        if !check
+            averages = qualifying_dice.map{ |d| d.average_roll }
+            max_avg_name = qualifying_dice.find{ |d| d.average_roll == averages.max }.description
+            min_avg_name = qualifying_dice.find{ |d| d.average_roll == averages.min }.description
+            top_max_perc = qualifying_dice.map{ |d| ((d.values.last.times_rolled / d.total_rolls.to_f) * 100).round(2) }.max
+            top_min_perc = qualifying_dice.map{ |d| ((d.values.first.times_rolled / d.total_rolls.to_f) * 100).round(2) }.max
+            top_max_name = qualifying_dice.find{ |d| ((d.values.last.times_rolled / d.total_rolls.to_f) * 100).round(2) == top_max_perc }.description
+            top_min_name = qualifying_dice.find{ |d| ((d.values.first.times_rolled / d.total_rolls.to_f) * 100).round(2) == top_min_perc }.description
+        end
+            { 
+                max_avg: check ? "--" : averages.max,
+                max_avg_name: check ? "No qualifying dice" : max_avg_name,
+                min_avg: check ? "--" : averages.min,
+                min_avg_name: check ? "No qualifying dice" : min_avg_name,
+                max_perc: check ? "--" : top_max_perc,
+                max_perc_name: check ? "No qualifying dice" : top_max_name,
+                min_perc: check ? "--" : top_min_perc,
+                min_perc_name: check ? "No qualifying dice" : top_min_name
+            }
     end
 end
--- frontend/src/components/Stats.js
+++ frontend/src/components/Stats.js
@@ -17,74 +17,6 @@ const Stats = () => {
   
   const totalDice = (type) => dice.filter(d => d.type_of_die === type).length
   const totalRolls = (type) => dice.filter(d => d.type_of_die === type).map(d => d.total_rolls).reduce((a, b) => a + b, 0)
-  // const twenties = dice.filter(d => d.type_of_die === "d20").map(d => d.id)
-  // const nattyOnes = values.filter(v => twenties.includes(v.die_id)).filter(v => v.value === 1).map(v => v.times_rolled).reduce((a, b) => a + b, 0)
-  // const nattyTwenties = values.filter(v => twenties.includes(v.die_id)).filter(v => v.value === 20).map(v => v.times_rolled).reduce((a, b) => a + b, 0)
-  // const craps = dice.filter(d => d.type_of_die === "2d6").map(d => d.id)
-  // const sevens = values.filter(v => craps.includes(v.die_id)).filter(v => v.value === 7).map(v => v.times_rolled).reduce((a, b) => a + b, 0)
-  // const sevenPercentage = Math.round(((sevens / totalRolls("2d6")) * 100) * 100) / 100 
-  // const highestAvg = (type) => {
-  //   const eligibleDice = dice.filter(d => d.type_of_die === type && d.total_rolls >= 50)
-  //   const averages = eligibleDice.map(d => d.average_roll)
-  //   const max = Math.max(...averages)
-  //   if (max === -Infinity) {
-  //     return "N/A"
-  //   }
-  //   const desc = dice.find(d => d.average_roll === max).description
-  //   return (
-  //     <><b>{max}</b><br/><i>{desc}</i></>
-  //     )
-  // }
-  // const lowestAvg = (type) => {
-  //   const eligibleDice = dice.filter(d => d.type_of_die === type && d.total_rolls >= 50)
-  //   const averages = eligibleDice.map(d => d.average_roll)
-  //   const min = Math.min(...averages)
-  //   if (min === Infinity) {
-  //     return "N/A"
-  //   }
-  //   const desc = dice.find(d => d.average_roll === min).description
-  //   return (
-  //     <><b>{min}</b><br/><i>{desc}</i></>
-  //     )
-  // }
-  // const maxValPer = (type) => {
-  //   const eligibleDice = dice.filter(d => d.type_of_die === type && d.total_rolls >= 50)
-  //   if (eligibleDice.length === 0) {
-  //     return "N/A"
-  //   } else {
-  //     const maxValues = eligibleDice.map(d => {
-  //       return [d.description, d.total_rolls, d.values]
-  //       }
-  //     )
-  //     const timesRolledArr = maxValues.map(d => d[2][d[2].length - 1].times_rolled)
-  //     const max = Math.max(...timesRolledArr)
-  //     const maxArr = maxValues.find(arr => arr[2][arr[2].length - 1].times_rolled === max)
-  //     const percentage = Math.round(((max / maxArr[1]) * 100) * 100) / 100 
-  //     const desc = maxArr[0]
-  //     return (
-  //       <><b>{percentage}%</b><br/><i>{desc}</i></>
-  //     )
-  //   }
-  // }
-  // const minValPer = (type) => {
-  //   const eligibleDice = dice.filter(d => d.type_of_die === type && d.total_rolls >= 50)
-  //   if (eligibleDice.length === 0) {
-  //     return "N/A"
-  //   } else {
-  //     const minValues = eligibleDice.map(d => {
-  //       return [d.description, d.total_rolls, d.values]
-  //       }
-  //     )
-  //     const timesRolledArr = minValues.map(d => d[2][0].times_rolled)
-  //     const min = Math.max(...timesRolledArr)
-  //     const minArr = minValues.find(arr => arr[2][0].times_rolled === min)
-  //     const percentage = Math.round(((min / minArr[1]) * 100) * 100) / 100 
-  //     const desc = minArr[0]
-  //     return (
-  //       <><b>{percentage}%</b><br/><i>{desc}</i></>
-  //     )
-  //   }
-  // }
 
   return (
     <div className="stats">
@@ -111,7 +43,7 @@ const Stats = () => {
               </tr>
               <tr>
                 <td>Seven Percentage (2d6)</td>
-                <td>{isNaN(seven_perc) ? "N/A" : `${seven_perc}%`}</td>
+                <td>{!seven_perc ? "N/A" : `${seven_perc}%`}</td>
               </tr>
               </tbody>
           </Table>
@@ -180,26 +112,26 @@ const Stats = () => {
               <tr>
                 <td>Maximum Roll Percentage<br/>
                 (min. 50 rolls)</td>
-                <td title={`Highest Percentage of "4" Rolls`}>{type_stats["d4"].max_perc}<br/><i>{type_stats["d4"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "6" Rolls`}>{type_stats["d6"].max_perc}<br/><i>{type_stats["d6"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "8" Rolls`}>{type_stats["d8"].max_perc}<br/><i>{type_stats["d8"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "0" Rolls`}>{type_stats["d10"].max_perc}<br/><i>{type_stats["d10"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "90" Rolls`}>{type_stats["d%"].max_perc}<br/><i>{type_stats["d%"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "12" Rolls`}>{type_stats["d12"].max_perc}<br/><i>{type_stats["d12"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "20" Rolls`}>{type_stats["d20"].max_perc}<br/><i>{type_stats["d20"].max_perc_name}</i></td>
-                <td title={`Highest Percentage of "12" Rolls`}>{type_stats["2d6"].max_perc}<br/><i>{type_stats["2d6"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "4" Rolls`}>{type_stats["d4"].max_perc}%<br/><i>{type_stats["d4"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "6" Rolls`}>{type_stats["d6"].max_perc}%<br/><i>{type_stats["d6"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "8" Rolls`}>{type_stats["d8"].max_perc}%<br/><i>{type_stats["d8"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "0" Rolls`}>{type_stats["d10"].max_perc}%<br/><i>{type_stats["d10"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "90" Rolls`}>{type_stats["d%"].max_perc}%<br/><i>{type_stats["d%"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "12" Rolls`}>{type_stats["d12"].max_perc}%<br/><i>{type_stats["d12"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "20" Rolls`}>{type_stats["d20"].max_perc}%<br/><i>{type_stats["d20"].max_perc_name}</i></td>
+                <td title={`Highest Percentage of "12" Rolls`}>{type_stats["2d6"].max_perc}%<br/><i>{type_stats["2d6"].max_perc_name}</i></td>
               </tr>
               <tr>
                 <td>Minimum Roll Percentage<br/>
                 (min. 50 rolls)</td>
-                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d4"].min_perc}<br/><i>{type_stats["d4"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d6"].min_perc}<br/><i>{type_stats["d6"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d8"].min_perc}<br/><i>{type_stats["d8"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d10"].min_perc}<br/><i>{type_stats["d10"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "00" Rolls`}>{type_stats["d%"].min_perc}<br/><i>{type_stats["d%"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d12"].min_perc}<br/><i>{type_stats["d12"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d20"].min_perc}<br/><i>{type_stats["d20"].min_perc_name}</i></td>
-                <td title={`Highest Percentage of "2" Rolls`}>{type_stats["2d6"].min_perc}<br/><i>{type_stats["2d6"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d4"].min_perc}%<br/><i>{type_stats["d4"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d6"].min_perc}%<br/><i>{type_stats["d6"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d8"].min_perc}%<br/><i>{type_stats["d8"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d10"].min_perc}%<br/><i>{type_stats["d10"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "00" Rolls`}>{type_stats["d%"].min_perc}%<br/><i>{type_stats["d%"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d12"].min_perc}%<br/><i>{type_stats["d12"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "1" Rolls`}>{type_stats["d20"].min_perc}%<br/><i>{type_stats["d20"].min_perc_name}</i></td>
+                <td title={`Highest Percentage of "2" Rolls`}>{type_stats["2d6"].min_perc}%<br/><i>{type_stats["2d6"].min_perc_name}</i></td>
               </tr>
             </tbody>
           </Table>
