0e264a69f686d959af3d99d9336e4c1fd2078f5d
Donte Ladatto <donte279@gmail.com>
2023-07-30 10:29:40 -0500

    move dice fetch to context

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,20 +3,9 @@ import NavBar from './NavBar';
 import Home from './Home';
 import Dice from './Dice';
 import Stats from './Stats';
-import { useContext, useEffect } from 'react';
 import { Route, Switch } from "react-router-dom";
-import { DiceContext } from "../context/dice";
 
 function App() {
-
-  const { setDice } = useContext(DiceContext)
-
-  useEffect(() => {
-    fetch("http://localhost:9292/dice")
-    .then(r => r.json())
-    .then(data => setDice(data))
-    .catch(err => alert("Could not access database. Make sure the server at http://localhost:9292 is running!"))
-  }, [])
 
   return (
     <div className="App">
--- frontend/src/context/dice.js
+++ frontend/src/context/dice.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const DiceContext = React.createContext();
 
 function DiceProvider({ children }) {
-    
-    const [dice, setDice] = useState([])
-    const [lastValue, setLastValue] = useState(false)
 
-    return <DiceContext.Provider value={{ dice, setDice, lastValue, setLastValue }}>{children}</DiceContext.Provider>
+	const [dice, setDice] = useState([])
+	const [lastValue, setLastValue] = useState(false)
+
+	useEffect(() => {
+		fetch("http://localhost:9292/dice")
+			.then(r => r.json())
+			.then(data => setDice(data))
+			.catch(err => alert("Could not access database. Make sure the server at http://localhost:9292 is running!"))
+	}, [])
+
+	return <DiceContext.Provider value={{ dice, setDice, lastValue, setLastValue }}>{children}</DiceContext.Provider>
 }
 
-export { DiceContext, DiceProvider} 
+export { DiceContext, DiceProvider } 
